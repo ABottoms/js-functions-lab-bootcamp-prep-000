328e488654c5a061b85e8d1909735a4dfac04fc5
salubrious-chain-5511 <anisa.bottoms@gmail.com>
2019-06-10 19:15:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 {}
-("HappyHolidays!)
+function say ("Happy Holidays!)
 {}
-
+function say ('Happy Holidays to')
+ return (string) 'Happy Holidays, ${name}!'
+ {}
+function say ('Happy ${holiday}, ${name}!)
+ {}
+('Holiday Countdown')
+function say ('It's ${days} until ${holiday}!')
+ 
 // write your code below!
